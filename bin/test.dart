2163b573abbe4deac88729e5b25dabc8e752b070
rbtsoheib <taharsoheib08@gmail.com>
2026-02-22 12:02:02 +0100

    FINALLY , IT IS WORKING , HALLALUJA

--- bin/test.dart
+++ bin/test.dart
@@ -1,3 +1,15 @@
 void main(List<String> arguments) {
   print("hello world");
+
+  String Name = "Soheib l3ziiiiiz";
+
+  int Age = 18;
+
+  const String anime = " Well , Attack on titan of course";
+
+  // bool isOpen = true ;
+
+  print(
+    "Heyyy, my name is $Name ,I am a little young you know , I am $Age , and my favortie anime was HXH , but now $anime",
+  );
 }
