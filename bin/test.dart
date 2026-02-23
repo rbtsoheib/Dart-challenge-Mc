6129ea4320a7c04906c8b60b441566f608bf4b64
rbtsoheib <taharsoheib08@gmail.com>
2026-02-23 14:15:21 +0100

    returned the first code

--- bin/test.dart
+++ bin/test.dart
@@ -1,26 +1,25 @@
-
 void main(List<String> arguments) {
-  //   print("hello world");
+    print("hello world");
 
-  //   String Name = "Soheib l3ziiiiiz";
+    String Name = "Soheib l3ziiiiiz";
 
-  //   int Age = 18;
+    int Age = 18;
 
-  //   const String anime = " Well , AOT of course";
+    const String anime = " Well , AOT of course";
 
-  //   // bool isOpen = true ;
+    // bool isOpen = true ;
 
-  //   print(
-  //     "Heyyy, my name is $Name ,I am a little young you know , I am $Age , and my favortie anime was HXH , but now $anime",
-  //   );
+    print(
+      "Heyyy, my name is $Name ,I am a little young you know , I am $Age , and my favortie anime was HXH , but now $anime",
+    );
 
-  //   final greeting = greet("Soso, YAYA, WASSI ", 18) ;
-  //   print(greeting) ;
-  // }
+    final greeting = greet("Soso, YAYA, WASSI ", 18) ;
+    print(greeting) ;
+  }
 
-  // greet (String name, int age) {
+  greet (String name, int age) {
 
-  // return "our names are $name and we are $age years old" ;
+  return "our names are $name and we are $age years old" ;
 
   for (int i = 0; i < 5; i++) {
     print("The value of i is: $i");
