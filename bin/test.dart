b60c301da8dfc07872603d3c5b7978f9416ef772
rbtsoheib <taharsoheib08@gmail.com>
2026-02-23 14:34:23 +0100

    experimenting

--- bin/test.dart
+++ bin/test.dart
@@ -1,40 +1,43 @@
 void main(List<String> arguments) {
-    print("hello world");
+  //   print("hello world");
 
-    String Name = "Soheib l3ziiiiiz";
+  //   String Name = "Soheib l3ziiiiiz";
 
-    int Age = 18;
+  //   int Age = 18;
 
-    const String anime = " Well , AOT of course";
+  //   const String anime = " Well , AOT of course";
 
 
-    print(
-      "Heyyy, my name is $Name ,I am a little young you know , I am $Age , and my favortie anime was HXH , but now $anime",
-    );
+  //   print(
+  //     "Heyyy, my name is $Name ,I am a little young you know , I am $Age , and my favortie anime was HXH , but now $anime",
+  //   );
 
-    final greeting = greet("Soso, YAYA, WASSI ", 18) ;
-    print(greeting) ;
+  //   final greeting = greet("Soso, YAYA, WASSI ", 18) ;
+  //   print(greeting) ;
 
-   for (int i = 0; i < 5; i++) {
-    print("The value of i is: $i");
+  //  for (int i = 0; i < 5; i++) {
+  //   print("The value of i is: $i");
+  // }
+
+  // for (double s = 2.0; s < 10.0; s++) {
+  //   print("the value of s is : $s");
+  // }
+
+  // for (String name in ["soheib", "yassine", "wassim"]) {
+  //   print("the name is : $name");
+  // } 
+  // for (String Names in {"Taharfiletypes" , "soheibslabyrinth" , "rt.soheiv" }) {
+  //   print("the chosen names are $Names") ; 
+  // }
+
+  var name = "Sexyass-soheib" ; 
+  print(name) ; 
+
   }
 
-  for (double s = 2.0; s < 10.0; s++) {
-    print("the value of s is : $s");
-  }
+  // greet (String name, int age) {
 
-  for (String name in ["soheib", "yassine", "wassim"]) {
-    print("the name is : $name");
-  } 
-  for (String Names in {"Taharfiletypes" , "soheibslabyrinth" , "rt.soheiv" }) {
-    print("the chosen names are $Names") ; 
-  }
+  // return "our names are $name and we are $age years old" ;
 
-  }
 
-  greet (String name, int age) {
-
-  return "our names are $name and we are $age years old" ;
-
- 
-}
+// }
